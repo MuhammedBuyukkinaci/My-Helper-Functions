cd0a8c9a558bba5b7ee8531d553a717044f46c71
Muhammed Buyukkinaci <33747602+MuhammedBuyukkinaci@users.noreply.github.com>
2019-04-23 21:41:41 +0300

    Update Helper_Functions.py

--- Helper_Functions.py
+++ Helper_Functions.py
@@ -160,3 +160,34 @@ def seed_everything(seed=1234):
     #torch.manual_seed(seed)
     #torch.cuda.manual_seed(seed)
     #torch.backends.cudnn.deterministic = True
+
+#Convnet on PyTorch
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchsummary import summary
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, kernel_size=5)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(16 * 5 * 5 , 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = F.max_pool2d(self.conv1(x), 2)
+        x = F.relu(x)
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 16 * 5 * 5 )
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # PyTorch v0.4.0
+model = Net().to(device)
+
+summary(model, (3, 32, 32))
+
