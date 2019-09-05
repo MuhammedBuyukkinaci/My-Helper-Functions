b621989c2edaa36475f5fc63779d621c58709f24
Muhammed Buyukkinaci <33747602+MuhammedBuyukkinaci@users.noreply.github.com>
2019-09-05 22:39:52 +0300

    resumetable
    resumetable

--- helper_functions.py
+++ helper_functions.py
@@ -292,6 +292,23 @@ for col, values in df_train.iteritems():
     print (values.unique())
     print ('\n')
 
+#Resumetable
+from scipy import stats
+def resumetable(df):
+    print(f"Dataset Shape: {df.shape}")
+    summary = pd.DataFrame(df.dtypes,columns=['dtypes'])
+    summary = summary.reset_index()
+    summary['Name'] = summary['index']
+    summary = summary[['Name','dtypes']]
+    summary['Missing'] = df.isnull().sum().values    
+    summary['Uniques'] = df.nunique().values
+    summary['First Value'] = df.loc[0].values
+    summary['Second Value'] = df.loc[1].values
+    summary['Third Value'] = df.loc[2].values
 
+    for name in summary['Name'].value_counts().index:
+        summary.loc[summary['Name'] == name, 'Entropy'] = round(stats.entropy(df[name].value_counts(normalize=True), base=2),2) 
+
+    return summary
 
 
